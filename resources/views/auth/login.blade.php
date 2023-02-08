8d96d8600501ff2b0dd408e077cc188c84dbfeed
unknown <CIT183G-21@M303.UC-BCF.EDU.PH>
2023-02-08 17:12:44 +0800

    renamed register route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
             @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Don't have an account?</a>
                         @endif
 
             <x-primary-button class="ml-3">
